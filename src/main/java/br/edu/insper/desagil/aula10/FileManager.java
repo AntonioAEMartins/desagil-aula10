7165de931073576f3dbea02d4429435942131ece
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-03-23 09:19:26 -0300

    Add answer

--- src/main/java/br/edu/insper/desagil/aula10/FileManager.java
+++ src/main/java/br/edu/insper/desagil/aula10/FileManager.java
@@ -8,24 +8,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class FileManager {
 	// Não precisa entender o código abaixo. (mas
 	// claro que pode perguntar se estiver curioso)
 
-	private List<String> log;
 	private CharsetDecoder decoder;
 
 	public FileManager() {
-		this.log = new ArrayList<>();
 		this.decoder = Charset.forName("UTF-8").newDecoder();
-	}
-
-	public List<String> getLog() {
-		return Collections.unmodifiableList(log);
 	}
 
 	// Não precisa entender o código acima. (mas
@@ -41,7 +32,6 @@ public class FileManager {
 				builder.append(line + '\n');
 			}
 			content = builder.toString();
-			log.add("Conteúdo lido");
 		} catch (FileNotFoundException exception) {
 			throw new LoadException("Arquivo não encontrado: " + exception.getMessage());
 		} catch (IOException readException) {
@@ -54,7 +44,6 @@ public class FileManager {
 	public void save(String path, String content) throws SaveException {
 		try (FileWriter writer = new FileWriter(path)) {
 			writer.write(content);
-			log.add("Conteúdo escrito");
 		} catch (IOException writeException) {
 			throw new SaveException("Erro de escrita: " + writeException.getMessage());
 		}
